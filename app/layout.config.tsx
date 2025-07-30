7be22c743ff6df28642f560bac2a0cc92e2016fe
Duke <emersonalmeidax@gmail.com>
2025-07-30 12:53:26 -0300

    change app name

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -19,7 +19,8 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        
+        Operion
       </>
     ),
   },
